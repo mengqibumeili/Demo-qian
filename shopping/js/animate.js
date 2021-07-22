46ddf2328293c61065cceda7b26429442d599f9a
chengqian <1274542905@qq.com>
2021-07-22 16:10:10 +0800

    添加日历组件

--- shopping/js/animate.js
+++ shopping/js/animate.js
@@ -1,5 +1,7 @@
 function animate(obj, target, callback) {
+    // 清除之前的定时器
     clearInterval(obj.timer);
+    // 创建定时器
     obj.timer = setInterval(function () {
         var step = (target - obj.offsetLeft) / 10;
             step = step > 0 ? Math.ceil(step) : Math.floor(step);
